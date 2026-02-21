13ab9a129e912b116436b1e2ed0c05200fa3ffb3
Nur Wachid <wachid@outlook.com>
2026-02-22 01:43:25 +0700

    add fix: page info

--- types/common.go
+++ types/common.go
@@ -1,3 +1,9 @@
 package types
 
 type Conditions map[string]interface{}
+
+// PageInfo represents simple offset-based pagination request
+type PageInfo struct {
+	PageNumber int `form:"pageNumber" json:"pageNumber"`
+	PageSize   int `form:"pageSize" json:"pageSize"`
+}
